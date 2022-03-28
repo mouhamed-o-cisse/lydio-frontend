3b6d69aa79ea4afe63f9762f2a4d7d306b0c3113
Mouhamed <golf452013@gmail.com>
2022-03-28 00:48:28 +0000

    commit

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express');
+// const express = require('express');
+
+// const path = require('path');
+
+// const app = express();
+
+// app.use(express.static(__dirname + '/build/'));
+
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/build/index.html'));});
+
+// app.listen(process.env.PORT || 1212);
+
+// console.log('Server started');
 
 const path = require('path');
-
+const express = require('express');
 const app = express();
-
-app.use(express.static(__dirname + '/build/'));
-
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/build/index.html'));});
-
-app.listen(process.env.PORT || 1212);
-
-console.log('Server started');
+const publicPath = path.join(__dirname, '..', 'app/build');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log('Server is up!');
+});
